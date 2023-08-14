191174f4856ee6101801931b4556f2f1d1697d31
Juzekkk <juzekgaming@gmail.com>
2023-08-14 11:30:37 +0200

    3 commands added, some refactoring

--- main.js
+++ main.js
@@ -31,59 +31,106 @@ var import_obsidian = require("obsidian");
 
 class AutoLinker extends Plugin {
   async onload() {
-    // Register the command
+    this.registerCommands();
+  }
+
+  registerCommands() {
     this.addCommand({
-      id: "convert-titles-to-links",
-      name: "Convert Titles to Obsidian Links",
-      callback: () => this.convertTitlesToLinks(),
+      id: "link-all-titles-in-vault",
+      name: "Link all titles within the entire vault",
+      callback: () => {
+        const allTitles = this.getAllFileTitles();
+        this.linkTitlesAcrossAllFiles(allTitles);
+        new Notice("All titles in the vault have been linked!");
+      },
+    });
+
+    this.addCommand({
+      id: "link-titles-in-current-file",
+      name: "Link titles within the current file and to the current file from other files",
+      callback: () => {
+        const currentFile = this.app.workspace.getActiveFile();
+        if (!currentFile) {
+          new Notice("No active file found!");
+          return;
+        }
+
+        const otherTitles = this.getAllFileTitles().filter(
+          (title) => title !== currentFile.basename
+        );
+        this.linkTitlesWithinFile(currentFile, otherTitles);
+        this.linkCurrentFileTitleAcrossAllFiles(currentFile.basename);
+
+        new Notice("Titles in the current file have been linked!");
+      },
+    });
+
+    this.addCommand({
+      id: "link-titles-within-current-file",
+      name: "Link titles only within the current file",
+      callback: () => {
+        const currentFile = this.app.workspace.getActiveFile();
+        if (!currentFile) {
+          new Notice("No active file found!");
+          return;
+        }
+
+        const titlesInCurrentFile = this.getAllFileTitles().filter(
+          (title) => title !== currentFile.basename
+        );
+        this.linkTitlesWithinFile(currentFile, titlesInCurrentFile);
+
+        new Notice("Titles within the current file have been linked!");
+      },
+    });
+
+    this.addCommand({
+      id: "link-current-title-in-vault",
+      name: "Link the current file's title across the entire vault",
+      callback: () => {
+        const currentFile = this.app.workspace.getActiveFile();
+        if (!currentFile) {
+          new Notice("No active file found!");
+          return;
+        }
+
+        this.linkCurrentFileTitleAcrossAllFiles(currentFile.basename);
+        new Notice("Current file's title has been linked in the vault!");
+      },
     });
   }
 
-  convertTitlesToLinks() {
-    const vaultPath = this.app.vault.adapter.basePath;
-
-    const titles = this.getTitlesFromFilenames(vaultPath);
-    this.replaceTitlesWithLinks(vaultPath, titles);
-
-    new Notice("Titles have been converted to Obsidian links!");
+  getAllFileTitles() {
+    return this.app.vault.getMarkdownFiles().map((file) => file.basename);
   }
 
-  getTitlesFromFilenames(folderPath) {
-    let titles = [];
-    const files = this.app.vault.getMarkdownFiles();
-
-    for (const file of files) {
-      const title = file.basename; // Gets the filename without extension
-      titles.push(title);
-    }
-
-    return titles;
+  linkTitlesAcrossAllFiles(titles) {
+    const allFiles = this.app.vault.getMarkdownFiles();
+    allFiles.forEach((file) => this.linkTitlesWithinFile(file, titles));
   }
 
-  replaceTitlesWithLinks(folderPath, titles) {
-    const files = this.app.vault.getMarkdownFiles();
+  linkCurrentFileTitleAcrossAllFiles(title) {
+    this.linkTitlesAcrossAllFiles([title]);
+  }
 
-    for (const file of files) {
-      this.app.vault.read(file).then((content) => {
-        titles.forEach((title) => {
-          let obsidianLink = `[[${title}]]`;
-
-          // Create a regular expression with the 'i' flag for case-insensitive matching
-          let titleRegex = new RegExp(
-            `(?<!\\[\\[)${this.escapeRegExp(title)}(?!\\]\\])`,
-            "gi"
-          );
-
-          content = content.replace(titleRegex, obsidianLink);
-
-          this.app.vault.modify(file, content);
-        });
+  linkTitlesWithinFile(file, titles) {
+    this.app.vault.read(file).then((content) => {
+      titles.forEach((title) => {
+        const obsidianLink = `[[${title}]]`;
+        const titleRegex = new RegExp(
+          `(?<!\\[\\[)${this.escapeRegExp(title)}(?!\\]\\])`,
+          "gi"
+        );
+        content = content.replace(titleRegex, obsidianLink);
       });
-    }
+      this.app.vault.modify(file, content);
+    });
   }
 
-  // Helper function to escape special characters in the title for regex
+  // Helper function to escape special characters for regex
   escapeRegExp(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
   }
 }
+
+module.exports = AutoLinker;
